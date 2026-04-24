5be394645af9d0490ad5f010003f31dbdacb9d03
alokgaurav09-create <alokgaurav09@gmail.com>
2026-04-24 23:12:31 +0530

    update message

--- chatbot.py
+++ chatbot.py
@@ -2,10 +2,10 @@ import streamlit as st
 from groq import Groq
 
 # Guardrail lists
-blocked_inputs = ["ignore", "system prompt", "api key", "jailbreak", "bypass"]
+blocked_inputs = ["ignore","credentials","password", "system prompt", "api key", "jailbreak", "bypass"]
 blocked_outputs = ["secret", "api key", "system instructions"]
 
-st.title("Secure Chatbot with Guardrails")
+st.title("💬 Chatbot by Alok")
 
 api_key = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=api_key)
@@ -14,7 +14,13 @@ client = Groq(api_key=api_key)
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
-user_input = st.text_input("You:")
+# Show conversation history
+for msg in st.session_state["messages"]:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+# New input box
+user_input = st.chat_input("Type your message...")
 
 if user_input:
     # Input guardrail check
@@ -22,6 +28,8 @@ if user_input:
         st.error("⚠️ Unsafe input detected. Not allowed.")
     else:
         st.session_state["messages"].append({"role": "user", "content": user_input})
+        with st.chat_message("user"):
+            st.write(user_input)
 
         response = client.chat.completions.create(
             model="llama-3.1-8b-instant",
@@ -35,4 +43,5 @@ if user_input:
             st.error("⚠️ Unsafe output blocked.")
         else:
             st.session_state["messages"].append({"role": "assistant", "content": bot_reply})
-            st.write("Bot:", bot_reply)
+            with st.chat_message("assistant"):
+                st.write(bot_reply)
